1af551ac86dc1dbf2149f0f7a5e5db959c56a52d
snehakPV <sneha.sf4154@gmail.com>
2025-09-22 14:24:42 +0530

    Prepared sample for the OTP Input

---
+++ OTPInputSample/OTPInputSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace OTPInputSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OtpInput_ValueChanged(object sender, Syncfusion.Maui.Toolkit.OtpInput.OtpInputValueChangedEventArgs e)
+        {
+            string enteredOtp = e.NewValue;
+
+            if (string.IsNullOrEmpty(enteredOtp))
+            {
+                OtpInput.InputState = Syncfusion.Maui.Toolkit.OtpInput.OtpInputState.Default;
+            }
+            else if (enteredOtp.Length == OtpInput.Length)
+            {
+                if (enteredOtp == "1234")
+                {
+                    OtpInput.InputState = Syncfusion.Maui.Toolkit.OtpInput.OtpInputState.Success;
+                }
+                else
+                {
+                    OtpInput.InputState = Syncfusion.Maui.Toolkit.OtpInput.OtpInputState.Error;
+
+                }
+            }
+            else
+            {
+                OtpInput.InputState = Syncfusion.Maui.Toolkit.OtpInput.OtpInputState.Warning;
+            }
+        }
+    }
+}
